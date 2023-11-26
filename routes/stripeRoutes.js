c0274d0a1d3642c0f2bc1c7f4001c8088b70cc1c
Chromax-design <chromax.design@gmail.com>
2023-11-26 19:56:25 +0100

    editted

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
-const { CheckoutSession, webHook } = require("../controllers/stripeControllers");
+const {
+  CheckoutSession,
+  webHook,
+} = require("../controllers/stripeControllers");
 const stripeRouter = express.Router();
 
 stripeRouter.post("/create-checkout-session", CheckoutSession);
-stripeRouter.post("/webhook", webHook)
+stripeRouter.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  express.raw({ type: "application/json" }),
+  webHook
+);
 module.exports = stripeRouter;
