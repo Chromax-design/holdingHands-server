8d26c2b608edfdd9e32c5f926b0e04ea72a241fd
Chromax-design <chromax.design@gmail.com>
2023-12-01 08:29:17 +0100

    editted

--- controllers/stripeControllers.js
+++ controllers/stripeControllers.js
@@ -49,57 +49,8 @@ const StripeWebhook = (req, res) => {
     switch (event.type) {
       case "checkout.session.completed":
         console.log(userId);
-        console.log(session)
+        console.log(session);
         break;
-      case "payment_intent.amount_capturable_updated":
-        const paymentIntentAmountCapturableUpdated = event.data.object;
-        console.log(paymentIntentAmountCapturableUpdated);
-        // Then define and call a function to handle the event payment_intent.amount_capturable_updated
-        break;
-      case "payment_intent.canceled":
-        const paymentIntentCanceled = event.data.object;
-        console.log(paymentIntentCanceled);
-        // Then define and call a function to handle the event payment_intent.canceled
-        break;
-      case "payment_intent.created":
-        const paymentIntentCreated = event.data.object;
-        console.log("created");
-        // Then define and call a function to handle the event payment_intent.created
-        break;
-      case "payment_intent.partially_funded":
-        const paymentIntentPartiallyFunded = event.data.object;
-        console.log(paymentIntentPartiallyFunded);
-        // Then define and call a function to handle the event payment_intent.partially_funded
-        break;
-      case "payment_intent.payment_failed":
-        const paymentIntentPaymentFailed = event.data.object;
-        console.log(paymentIntentPaymentFailed);
-        // Then define and call a function to handle the event payment_intent.payment_failed
-        break;
-      case "payment_intent.processing":
-        const paymentIntentProcessing = event.data.object;
-        console.log(paymentIntentProcessing);
-        // Then define and call a function to handle the event payment_intent.processing
-        break;
-      case "payment_intent.requires_action":
-        const paymentIntentRequiresAction = event.data.object;
-        console.log(paymentIntentRequiresAction);
-
-        // Then define and call a function to handle the event payment_intent.requires_action
-        break;
-      case "payment_intent.succeeded":
-        const paymentIntentSucceeded = event.data.object;
-        const dbObject = {
-          paymentId: paymentIntentSucceeded.id,
-          paymentStatus: paymentIntentSucceeded.status,
-          amountPaid: paymentIntentSucceeded.amount,
-          clientId: userId,
-        };
-        console.log(dbObject);
-        //   console.log(session)
-        // Then define and call a function to handle the event payment_intent.succeeded
-        break;
-      // ... handle other event types
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
