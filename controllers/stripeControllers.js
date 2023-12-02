27f41cde8de9b9902b545f7ebc0fb45b9f5f3fe6
Chromax-design <chromax.design@gmail.com>
2023-12-02 05:14:32 +0100

    editted

--- controllers/stripeControllers.js
+++ controllers/stripeControllers.js
@@ -1,4 +1,4 @@
-const { insertData } = require("../utils/sqlHandlers");
+const { insertData, updateData } = require("../utils/sqlHandlers");
 
 const stripe = require("stripe")(
   "sk_test_51N2arXIYmnZ4DnJJvdhuSNisgQ3UPhiAC7ZP9YmvKBlMSwNvw713RRa2XJ3JKYTOuMq1Duzs19PCVDsvdZjL3Kyt00engCA6v9"
@@ -27,29 +27,36 @@ const StripeCheckout = async (req, res) => {
       client_reference_id: product.menteeId,
     });
 
-    const checkObject = {
-      amount: product.price,
+    const checkOutObject = {
       mentor_Id: product.mentorId,
       mentee_Id: product.menteeId,
+      amount: product.price,
     };
-    // await insertData("subscription", checkObject);
 
-    console.log(checkObject);
+    await insertData("subscription", checkOutObject);
+    console.log(checkOutObject);
+
     res.json({ id: session.id });
   } catch (error) {
     console.log(error);
   }
 };
 
-const StripeWebhook = (req, res) => {
+const StripeWebhook = async (req, res) => {
   const event = req.body;
   const session = event.data.object;
   const userId = session.client_reference_id;
   try {
     switch (event.type) {
       case "checkout.session.completed":
-        console.log(userId);
-        console.log(session);
+        const webHookData = {
+          payment_Id: session.payment_intent,
+          payment_status: session.payment_status,
+        };
+
+        await updateData("subscription", webHookData, "mentee_Id", userId);
+
+        console.log(webHookData);
         break;
       default:
         console.log(`Unhandled event type ${event.type}`);
@@ -58,7 +65,7 @@ const StripeWebhook = (req, res) => {
     console.log(error);
   }
 
-  res.json({ received: true });
+  res.json({ received: true, message: "Payment was successful" });
 };
 
 module.exports = {
